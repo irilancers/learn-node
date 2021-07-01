6a71de28afee3804331a92f0d2ac86f3d899665c
irilancers <irilancers@gmail.com>
2021-07-01 18:28:09 +0430

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send('Hello World !!!')
+});
+
+app.post('/',  (req, res)=> {
+    const date = new Date();
+    res.send(`Got a POST request: ${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`)
+  });
+
+app.put('/updateProduction',  (req, res)=> {
+    res.send('updateProduction')
+  });
+
+  app.put('/updateCustomers',  (req, res)=> {
+    res.send('updateCustomers')
+  });
+
+
+app.delete('/', function (req, res) {
+    res.send('Got a DELETE request at /user')
+  })  
+
+app.listen(3000,()=>{console.log(`server is running on port 3000`);});
